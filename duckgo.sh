8360bb2c28c9f234b15586598bb88856688df80f
kafene <kafene@users.noreply.github.com>
2013-09-29 14:27:53 -0400

    Create duckgo.sh

---
+++ duckgo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# curl -s http://api.duckduckgo.com/\?no_html\=1\&format\=xml\&q\="$*" | hxselect -c "Answer";
+
+local response=$(curl -s http://api.duckduckgo.com/\?no_html\=1\&format\=xml\&q\="$*");
+local result;
+local found;
+
+# Try to get "Abstract"
+found="Abstract";
+result=$(echo -e "$response" | hxselect -c "Abstract");
+
+# If Abstract is empty, try to get "Definition"
+if [ -z "$result" ]; then
+    found="Definition";
+    result=$(echo -e "$response" | hxselect -c 'Definition');
+fi
+
+# If Definition is empty, try to get all "Text" results
+if [ -z "$result" ]; then
+    found="Text";
+    result=$(echo -e "$response" | hxselect -c -s '\n\n' 'Text');
+fi
+
+if [ -z "$result" ]; then
+    found="";
+    result="No results.";
+fi
+
+[ -n "$found" ] && echo "Results from '$found'.";
+echo -e "$result";
+echo;
